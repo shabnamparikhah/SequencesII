a74b65fdc50f7b44776d6873a1172afc7c0b740d
shabnam <sparikhah@gmail.com>
2018-07-11 16:55:40 -0400

    SequencesII

---
+++ src/SequencesII.java
@@ -0,0 +1,12 @@
+public class SequencesII {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Cost \t Quantity  \t Total");
+        System.out.println("$1,000.00 \t 4  \t $4,000.00");
+        System.out.println("$   50.00 \t 8  \t $400.00");
+        System.out.println("----------------------------");
+        System.out.print("TOTAL: \t\t 12 \t $400.00");
+
+     }
+}
